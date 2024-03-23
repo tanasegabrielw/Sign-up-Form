88be59749d2dd5b2706a62b4eb755e051af8bd91
Gabriel <72333512+tanasegabrielw@users.noreply.github.com>
2024-03-23 05:55:49 +0200

    feat: add password validation in JS

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Photo by <a href="https://unsplash.com/@haliewestphoto?utm_content=creditCopyTex
 [Background Image](https://www.youtube.com/watch?v=zHZRFwWQt2w)
 [HSL COLOR and OPACITY](https://www.w3schools.com/cssref/func_hsla.php)
 [Custom-font](https://www.w3schools.com/css/css3_fonts.asp)
+[DOM Manipulation and Events](https://www.theodinproject.com/lessons/foundations-dom-manipulation-and-events)
+[Input Event Listener](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event)
---
+++ code.js
@@ -0,0 +1,21 @@
+const password=document.querySelector('#psw');
+const confirmPassword=document.querySelector('#cpsw');
+const errorMessage=document.querySelector('.message');
+const errorClass=document.querySelector('.error');
+password.classList.remove('error');
+confirmPassword.classList.remove('error');
+
+function validatePassword(){
+    if(password.value!==confirmPassword.value){
+        errorMessage.textContent="Passwords don't match!";
+        password.classList.add('error');
+        confirmPassword.classList.add('error');
+    } else{
+        errorMessage.textContent='';
+        password.classList.remove('error');
+        confirmPassword.classList.remove('error');
+    }
+}
+
+password.addEventListener('input',validatePassword);
+confirmPassword.addEventListener('input',validatePassword);
